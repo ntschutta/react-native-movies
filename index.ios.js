85bc8fca942002d5d5f5735d6fad68d77cff79b8
Nate Schutta <ntschutta@gmail.com>
2015-10-25 15:57:51 -0500

    updated to get data instead of using mocked data

--- index.ios.js
+++ index.ios.js
@@ -5,6 +5,12 @@
 'use strict';
 var React = require('react-native');
 
+/**
+ * For quota reasons we replaced the Rotten Tomatoes' API with a sample data of
+ * their very own API that lives in React Native's Github repo.
+ */
+var REQUEST_URL = 'https://raw.githubusercontent.com/facebook/react-native/master/docs/MoviesExample.json';
+
 var MOCKED_MOVIES_DATA = [
   {title: 'Title',
     year: '2015',
@@ -21,8 +27,45 @@ var {
 } = React;
 
 var MoviesRock = React.createClass({
+  getInitialState: function() {
+    return {
+      movies: null,
+    };
+  },
+  componentDidMount: function() {
+    this.fetchData();
+  },
+
+  fetchData: function() {
+    fetch(REQUEST_URL)
+      .then((response) => response.json())
+      .then((responseData) => {
+        this.setState({
+          movies: responseData.movies,
+        });
+      })
+      .done();
+  },
   render: function() {
-      var movie = MOCKED_MOVIES_DATA[0];
+    if (!this.state.movies) {
+      return this.renderLoadingView();
+    }
+
+    var movie = this.state.movies[0];
+    return this.renderMovie(movie);
+  },
+
+  renderLoadingView: function() {
+    return (
+      <View style={styles.container}>
+        <Text>
+          Loading movies...
+        </Text>
+      </View>
+    );
+  },
+
+  renderMovie: function(movie) {
       return (
         <View style={styles.container}>
           <Image
